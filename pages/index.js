bb2bbb1afda5e7120a5d9726d4a6ff4effb95c4f
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2021-08-08 17:52:57 +0530

    visual changes

--- pages/index.js
+++ pages/index.js
@@ -36,14 +36,14 @@ export default function Home() {
         {/* splash */}
         <div className="flex flex-row pl-20 my-20">
           <div className="flex-row">
-            <p className="text-4xl font-bold text-white inline pr-4">
+            <p className="text-8xl font-bold text-white inline pr-4">
               Make podcasts accessible <br />
               for
             </p>
-            <p className="text-4xl font-bold text-white bg-highlight rounded  inline-block p-1">
+            <p className="text-8xl font-bold text-white bg-highlight rounded  inline-block p-1">
               everyone
             </p>
-            <p className="text-xl text-white my-5">- No exception.</p>
+            <p className="text-3xl text-white my-5">- No exception.</p>
             <button
               onClick={() => setShowSignIn(true)}
               className="bg-secondary font-bold inline-block rounded-lg text-white px-5 py-2 cursor-pointer mb-20"
@@ -86,7 +86,7 @@ export default function Home() {
           </div>
         </div>
         {/* Translate section */}
-        <div className="m-10 w-4/5 flex flex-row items-center justify-center">
+        <div className="m-10 mt-20 w-4/5 flex flex-row items-center justify-center">
           <div className="w-1/3">
             <Image src={Translate} alt="Translation" />
           </div>
@@ -102,7 +102,7 @@ export default function Home() {
           </div>
         </div>
         {/* Bookmark section */}
-        <div className="m-10 w-4/5 flex flex-row items-center justify-center mb-48">
+        <div className="m-10 mt-20 w-4/5 flex flex-row items-center justify-center mb-48">
           <div className="">
             <h1 className="text-4xl font-bold text-primary px-2 py-3 w-auto mt-10 mb-2 text-center">
               Bookmark Snippets
